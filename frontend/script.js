6efadab76b61d81bab7c4d0382191219362105c1
Lucas Nascimento <lucasthuba@gmail.com>
2024-10-01 16:05:06 -0300

    v1 do front

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+function coworking(){
+
+    var exibir = window.document.getElementById("result")
+    exibir.innerText = `lista de coworking`
+}
+
+function empresarial(){
+
+    var exibir = window.document.getElementById("result")
+    exibir.innerText = `lista das empresas`
+}
+
+function parceiros(){
+
+    var exibir = window.document.getElementById("result")
+    exibir.innerText = `lista dos parcerios`
+}
